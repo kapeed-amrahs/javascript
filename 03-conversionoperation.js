a18d93521cc6b785affa57fad5e64afd0d9d5d3b
Deepak Sharma <otw.deepak@gmail.com>
2025-10-30 21:24:14 +0530

    Conversion of data types

---
+++ 03-conversionoperation.js
@@ -0,0 +1,23 @@
+// CONVERSION CASE STUDY
+
+
+let score = "33abx"
+
+console.log(typeof score);
+
+let valueInNUmber = Number(score)
+console.log( valueInNUmber)
+
+// when you try to conver "33" which is a string=>it get converted to 33 which is a number.
+//but when we try to convert 33abc => Nan
+// true =>1;  false =>0;
+// WHEN YOU TRIED TO CONVERT ABOVE NUMBER IT CAN BE CONVERTED ... BUT YOU CAN NOT CHANGE ITS TYPE.
+ let isLoggedIn = 1
+
+ let BooleanIsLoggedIN= Boolean(isLoggedIn)
+ console.log(BooleanIsLoggedIN)
+
+ // IN THIS INVESATIGATION STUDY I FIND OUT THAT WHEN YOU TRY TO CONVERT 1 => TRUE IN BOOLEAN 
+ // BUT WHEN YOU TYPE 0=> FALSE 
+ //"" => FALSE 
+// "KAPEED"=> TRUE .
