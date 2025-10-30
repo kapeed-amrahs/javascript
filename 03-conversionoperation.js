f904ffc18350981261275a198fe210c611496b76
Deepak Sharma <otw.deepak@gmail.com>
2025-10-31 03:13:19 +0530

    operations

--- 03-conversionoperation.js
+++ 03-conversionoperation.js
@@ -21,3 +21,39 @@ console.log( valueInNUmber)
  // BUT WHEN YOU TYPE 0=> FALSE 
  //"" => FALSE 
 // "KAPEED"=> TRUE .
+
+
+// **************************************************OPERATIONS*****************************************************************************
+
+let value=3
+
+let negValue = -value
+
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2%3);
+
+
+let str1= "hello"
+let str2= " kapeed "
+
+let str3 = str1+str2;
+console.log(str3)
+
+console.log((3+4) * 5%4);
+
+console.log(+true);
+console.log(+"");
+
+let num1 ,num2 ,num3
+
+num1 =num2 =num3=2+2
+
+let gamecounter =100
+//++gamecounter;  this also works 
+gamecounter++; // this too
+console.log(gamecounter)
